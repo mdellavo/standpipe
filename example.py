c2659f3d4b39d5adc626bbc521d4c0894980f5be
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2018-07-19 20:40:51 -0400

    client reconnect, encoder, terminator

--- example.py
+++ example.py
@@ -1,16 +1,13 @@
+import logging
 import threading
-import json
 
 from standpipe import client, config
 
-RECORD_SEPARATOR = "\n"
 
 NUM_WORKERS = 5
 NUM_RECORDS = 100000
 
-
-def serialize(record):
-    return json.dumps(record) + RECORD_SEPARATOR
+logging.basicConfig(level=logging.DEBUG)
 
 
 def producer(i, n):
@@ -18,14 +15,15 @@ def producer(i, n):
 
     count = 0
     while count < n:
-        c.write("test-{}".format(i), serialize({"n": count}))
+        c.write("test-{}".format(i), {"i": i, "n": n, "count": count})
         count += 1
     c.close()
 
 
-threads = [threading.Thread(target=producer, args=(i, NUM_RECORDS)) for i in range(5)]
+threads = [threading.Thread(target=producer, args=(i, NUM_RECORDS)) for i in range(25)]
 
 for thread in threads:
+    thread.setDaemon(True)
     thread.start()
 
 for thread in threads:
--- standpipe/client.py
+++ standpipe/client.py
@@ -1,3 +1,6 @@
+import abc
+import json
+import time
 import socket
 import threading
 import logging
@@ -23,24 +26,45 @@ def drain(queue, sock):
 
 def worker(host, port, queue):
     while True:
-        with closing(socket.create_connection((host, port))) as sock:
+        for attempt in range(5):
+            log.info("connecting...")
             # noinspection PyBroadException
             try:
-                return drain(queue, sock)
+                with closing(socket.create_connection((host, port))) as sock:
+                    return drain(queue, sock)
             except Exception:
                 log.exception("worker died, restarting")
+            log.info("sleeping before reconnect...")
+            time.sleep(2**attempt)
+
+
+
+class Encoder(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def encode(self, event):
+        pass
+
+
+class JsonEncoder(Encoder):
+    def encode(self, event):
+        return json.dumps(event, sort_keys=True)
 
 
 class StreamClient(object):
-    def __init__(self, host, port, queue_size=CLIENT_QUEUE_SIZE):
+    def __init__(self, host, port, encoder=None, terminator=None, queue_size=CLIENT_QUEUE_SIZE):
         self.host = host
         self.port = port
         self.queue = Queue(maxsize=queue_size)
+        self.encoder = encoder or JsonEncoder()
+        self.terminator = terminator or u"\n"
         self.thread = threading.Thread(target=worker, args=(self.host, self.port, self.queue))
         self.thread.setDaemon(True)
         self.thread.start()
 
-    def write(self, stream_name, record):
+    def write(self, stream_name, event):
+        record = self.encoder.encode(event) + self.terminator
         self.queue.put((stream_name, record))
 
     def close(self):
